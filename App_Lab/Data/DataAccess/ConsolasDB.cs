eab00d52ead6a7e981f384ef369f96f64edd9f8c
Gabrielys21 <158766566+Gabrielys21@users.noreply.github.com>
2024-05-25 13:06:18 -0600

    Agregar archivos de proyecto.

---
+++ App_Lab/Data/DataAccess/ConsolasDB.cs
@@ -0,0 +1,131 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_Lab.Data.DataAccess
+{
+    internal class ConsolasDB
+    {
+        string connectionString = "server=localhost;Database=lab_class;Uid=root;Pwd=pasword";
+        MySqlConnection conection;
+
+        //constructor
+        public ConsolasDB()
+        {
+            conection = new MySqlConnection(connectionString);
+        }
+
+        public bool ProbarConexion()
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+        //insertar
+        public void Insertar(ConsolasDB consola)
+        {
+            try
+            {
+                string query = "INSERT INTO catalogo_consolas (nombre_consola, compania, anio_lanzamiento, generacion) VALUES (@NombreConsola, @Compania, @AnioLanzamiento, @Generacion)";
+                MySqlCommand cmd = new MySqlCommand(query, conection);
+                cmd.Parameters.AddWithValue("@NombreConsola", consola.NombreConsola);
+                cmd.Parameters.AddWithValue("@Compania", consola.Compania);
+                cmd.Parameters.AddWithValue("@AnioLanzamiento", consola.AnioLanzamiento);
+                cmd.Parameters.AddWithValue("@Generacion", consola.Generacion);
+                conection.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al agregar el registro: " + ex.Message);
+            }
+            finally
+            {
+                conection.Close();
+            }
+        }
+        //leer por id
+        public DataRow LeerId(int id)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                string query = "SELECT * FROM catalogo_consolas WHERE id_consola = @ID";
+                MySqlCommand cmd = new MySqlCommand(query, conection);
+                cmd.Parameters.AddWithValue("@ID", id);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                conection.Open();
+                adapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al leer el registro: " + ex.Message);
+            }
+            finally
+            {
+                conection.Close();
+            }
+            if (dt.Rows.Count > 0)
+            {
+                return dt.Rows[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public List<ConsolasDB> ObtenerConsolas()
+        {
+            List<ConsolasDB> consolas = new List<ConsolasDB>();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM catalogo_consolas";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                try
+                {
+                    connection.Open();
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        ConsolasDB consola = new ConsolasDB
+                        {
+                            IdConsola = reader.GetInt32("id_consola"),
+                            NombreConsola = reader.GetString("nombre_consola"),
+                            Compania = reader.GetString("compania"),
+                            AnioLanzamiento = reader.GetInt32("anio_lanzamiento"),
+                            Generacion = reader.GetByte("generacion")
+                        };
+
+                        consolas.Add(consola);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+
+            return consolas;
+        }
+
+    }
+}
---
+++ App_Lab/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace App_Lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1Nom = new System.Windows.Forms.Label();
+            this.labelCarne = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelnm = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1Nom
+            // 
+            this.label1Nom.AutoSize = true;
+            this.label1Nom.Font = new System.Drawing.Font("Consolas", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1Nom.Location = new System.Drawing.Point(12, 29);
+            this.label1Nom.Name = "label1Nom";
+            this.label1Nom.Size = new System.Drawing.Size(568, 40);
+            this.label1Nom.TabIndex = 0;
+            this.label1Nom.Text = "Angel Gabriel Chavarría Yanes";
+            // 
+            // labelCarne
+            // 
+            this.labelCarne.AutoSize = true;
+            this.labelCarne.Font = new System.Drawing.Font("Consolas", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCarne.Location = new System.Drawing.Point(586, 29);
+            this.labelCarne.Name = "labelCarne";
+            this.labelCarne.Size = new System.Drawing.Size(245, 40);
+            this.labelCarne.TabIndex = 1;
+            this.labelCarne.Text = "0905-23-3809";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(41, 203);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(138, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // labelnm
+            // 
+            this.labelnm.AutoSize = true;
+            this.labelnm.Font = new System.Drawing.Font("Consolas", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelnm.Location = new System.Drawing.Point(12, 105);
+            this.labelnm.Name = "labelnm";
+            this.labelnm.Size = new System.Drawing.Size(644, 40);
+            this.labelnm.TabIndex = 3;
+            this.labelnm.Text = "Jostyn Manrrique Godoy Chinchilla";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(674, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 40);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "0905-23-2991";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 605);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelnm);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelCarne);
+            this.Controls.Add(this.label1Nom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1Nom;
+        private System.Windows.Forms.Label labelCarne;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label labelnm;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
